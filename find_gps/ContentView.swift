bba7e9ac4fd193e74c0bc8112fc802d47c585045
bbulliard <bbulliard@gmail.com>
2022-03-24 18:26:47 -0500

    Initial Commit

---
+++ find_gps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  find_gps
+//
+//  Created by Bob Bulliard on 3/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ find_gps/find_gpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  find_gpsApp.swift
+//  find_gps
+//
+//  Created by Bob Bulliard on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct find_gpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
