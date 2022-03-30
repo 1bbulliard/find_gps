2226df5530bfc1729ba171354995eef239677db9
bbulliard <bbulliard@gmail.com>
2022-03-30 13:29:41 -0500

    20220330_1330

--- find_gps/ContentView.swift
+++ find_gps/ContentView.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreLocationUI
+
+import MapKit
+
+
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 30.20, longitude: -92.01),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
     }
 }
+
+final class ContentViewModel:ObservableObject {
+    var locationManager: CLLocationManager?
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
