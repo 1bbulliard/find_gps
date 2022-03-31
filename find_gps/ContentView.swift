d651e4f020ec040e0f4c10ed2a7cba54f8e68e1f
bbulliard <bbulliard@gmail.com>
2022-03-30 19:02:29 -0500

    20200330_1900

--- find_gps/ContentView.swift
+++ find_gps/ContentView.swift
@@ -14,22 +14,63 @@ import MapKit
 
 
 struct ContentView: View {
+    @StateObject private var viewModel = ContentViewModel()
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 30.20, longitude: -92.01),
                                                    span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
 
     var body: some View {
         Map(coordinateRegion: $region, showsUserLocation: true)
             .ignoresSafeArea()
+            .accentColor(Color(.systemPink))
+            .onAppear {
+                viewModel.checkiflocationservicesisenabled()
+                }
+    
     }
-}
 
-final class ContentViewModel:ObservableObject {
+}
+        
+class ContentViewModel: NSObject, ObservableObject,
+CLLocationManagerDelegate{
     var locationManager: CLLocationManager?
+    func checkiflocationservicesisenabled()
+    {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            //    checkLocationAuthorization()
+        } else {
+            print("show an alert")
+        }
+    }
+ func checkLocationAuthorization() {
+        guard let locationManager = locationManager else {return}
+        switch locationManager.authorizationStatus {
+        
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("your location is restricted maybe due to parental control")
+        case .denied:
+            print("you have denied permission for this app.. plz go into app allow")
+        case .authorizedAlways:
+                break
+        case
+          .authorizedWhenInUse:
+            break
+        @unknown default:
+            break
+        }
 }
-
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
